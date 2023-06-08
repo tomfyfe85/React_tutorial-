6db8f240247581dca134fe5807f59dee7e6799ea
Tom Fyfe <tomfyfe85@gmail.com>
2023-06-08 08:44:09 +0100

    added calculateWinner

--- tq2u88/src/App.js
+++ tq2u88/src/App.js
@@ -4,6 +4,9 @@ import { useState } from "react";
 
 export default function Board() {
   const handleClick = (i) => {
+    if (squares[i] || calculateWinner(squares)) {
+      return;
+    }
     const nextSquares = squares.slice();
     if (xIsNext) {
       nextSquares[i] = "X";
@@ -16,6 +19,8 @@ export default function Board() {
 
   const [xIsNext, setXIsNext] = useState(true);
   const [squares, setSquares] = useState(Array(9).fill(null));
+
+  
   return (
     <div>
       <div className="board-row">
@@ -35,4 +40,24 @@ export default function Board() {
       </div>
     </div>
   );
+
+  function calculateWinner(squares) {
+    const lines = [
+      [0, 1, 2],
+      [3, 4, 5],
+      [6, 7, 8],
+      [0, 3, 6],
+      [1, 4, 7],
+      [2, 5, 8],
+      [0, 4, 8],
+      [2, 4, 6]
+    ];
+    for (let i = 0; i < lines.length; i++) {
+      const [a, b, c] = lines[i];
+      if (squares[a] && squares[a] === squares[b] && squares[a] === squares[c]) {
+        return squares[a];
+      }
+    }
+    return null;
+  }
 }
