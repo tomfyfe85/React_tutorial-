1578763b7b845dcecd131bb04c1ea5246ac2275f
Tom Fyfe <tomfyfe85@gmail.com>
2023-06-27 22:32:55 +0100

    updated current squares in game

--- tq2u88/src/App.js
+++ tq2u88/src/App.js
@@ -5,8 +5,10 @@ import Square from "./Square";
 export default function Game() {
   const [xIsNext, setXIsNext] = useState(true);
   const [history, setHistory] = useState([Array(9).fill(null)]);
-  const currentSquares = history[history.length - 1];
+  // const currentSquares = history[history.length - 1];
   const [currentMove, setCurrentMove] = useState(0);
+    const currentSquares = history[currentMove];
+
 
   const handlePlay = (nextSquares) => {
     setHistory([...history, nextSquares]);
